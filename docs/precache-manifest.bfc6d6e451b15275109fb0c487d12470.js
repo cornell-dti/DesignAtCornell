03acf354d2b5dfd7d778270e2d204fb1423ef025
JBoss925 <jaggerbrulato@gmail.com>
2020-08-25 20:41:45 -0400

    added docs again, but this time added homepage attribute

---
+++ docs/precache-manifest.bfc6d6e451b15275109fb0c487d12470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8f7c00ac31fc9215cfca7b09e01be7",
+    "url": "/DesignAtCornell/index.html"
+  },
+  {
+    "revision": "61fe016441ce69bbedee",
+    "url": "/DesignAtCornell/static/css/main.f00cbd8d.chunk.css"
+  },
+  {
+    "revision": "57e400cd0adee22c9d02",
+    "url": "/DesignAtCornell/static/js/2.71c093eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DesignAtCornell/static/js/2.71c093eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61fe016441ce69bbedee",
+    "url": "/DesignAtCornell/static/js/main.4c0fd824.chunk.js"
+  },
+  {
+    "revision": "cc8ef901bdb360b7ee1c",
+    "url": "/DesignAtCornell/static/js/runtime-main.d961bc7a.js"
+  }
+]);
