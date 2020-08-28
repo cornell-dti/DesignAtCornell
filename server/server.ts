9ea07b3d89d3dd1628fd7286deca8e7b03afdbcc
JBoss925 <jaggerbrulato@gmail.com>
2020-08-28 16:52:14 -0400

    run server and build react in parallel so build step isn't blocked

--- server/server.ts
+++ server/server.ts
@@ -4,11 +4,11 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../docs/')));
+app.use(express.static(path.join(__dirname, '../design-at-cornell/build/')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../docs/', 'index.html'));
+  res.sendFile(path.join(__dirname, '../design-at-cornell/build/', 'index.html'));
 });
 
 app.listen(port, () => {
