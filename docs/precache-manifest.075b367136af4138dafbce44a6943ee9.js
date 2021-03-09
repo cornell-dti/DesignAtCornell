aa25a8f0c3effa1bfb4c69017f61d17338f452bf
Jessica <jessfeng@users.noreply.github.com>
2021-03-09 09:44:30 -0800

    Add Firebase Hosting and Github Actions (#15)
    * Add firebase init
    
    * Add firebase deployment
    
    * Add Github Actions for Firebase Hosting
    
    * Fix build error
    
    * Remove npm & firebase functions stuff, deploy frontend
    
    * yarn build
    
    * yarn install first
    
    * install in frontend
    
    * fix run command??
    
    * copy over my own PR yml
    
    * Remove Firebase Hosting Merge yaml
    
    Co-authored-by: Megan Yin <my474@cornell.edu>

---
+++ docs/precache-manifest.075b367136af4138dafbce44a6943ee9.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff47795f8036f33d5c084c790c448b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e01895af48ac2cc2679",
+    "url": "/static/css/2.7df41cd5.chunk.css"
+  },
+  {
+    "revision": "4a85a44a8679c2860451",
+    "url": "/static/css/4.ec3f976d.chunk.css"
+  },
+  {
+    "revision": "e284f2bcc60bf2a3172b",
+    "url": "/static/css/5.b70ade11.chunk.css"
+  },
+  {
+    "revision": "381b1e91160966ab553c",
+    "url": "/static/css/6.d061ae4f.chunk.css"
+  },
+  {
+    "revision": "24fb7591586443e12b64",
+    "url": "/static/css/7.7041401f.chunk.css"
+  },
+  {
+    "revision": "f894971b7202833e0896",
+    "url": "/static/css/8.8ba46d3c.chunk.css"
+  },
+  {
+    "revision": "72493934bc7c25ce1f36",
+    "url": "/static/css/main.f6cffc85.chunk.css"
+  },
+  {
+    "revision": "7e01895af48ac2cc2679",
+    "url": "/static/js/2.064776c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.064776c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03340291457107088b98",
+    "url": "/static/js/3.0391054b.chunk.js"
+  },
+  {
+    "revision": "4a85a44a8679c2860451",
+    "url": "/static/js/4.25cfb086.chunk.js"
+  },
+  {
+    "revision": "e284f2bcc60bf2a3172b",
+    "url": "/static/js/5.2c039efb.chunk.js"
+  },
+  {
+    "revision": "381b1e91160966ab553c",
+    "url": "/static/js/6.f5881cdf.chunk.js"
+  },
+  {
+    "revision": "24fb7591586443e12b64",
+    "url": "/static/js/7.58c7e7fd.chunk.js"
+  },
+  {
+    "revision": "f894971b7202833e0896",
+    "url": "/static/js/8.0456b5d0.chunk.js"
+  },
+  {
+    "revision": "c4e3f3262b13d765f94f",
+    "url": "/static/js/9.ed76e244.chunk.js"
+  },
+  {
+    "revision": "72493934bc7c25ce1f36",
+    "url": "/static/js/main.09b8aba5.chunk.js"
+  },
+  {
+    "revision": "d690909f7f8a9aa248e6",
+    "url": "/static/js/runtime-main.f8519dc3.js"
+  },
+  {
+    "revision": "ddbd34ec71dea010a5fdf2904bf64faa",
+    "url": "/static/media/404-background.ddbd34ec.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "4c4f4d2471d7dd36bdc83ebf463da12d",
+    "url": "/static/media/placeholder.4c4f4d24.png"
+  }
+]);
