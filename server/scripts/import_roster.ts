57a0d057762e1b688cd8bae9f6dcd444b3f7ce44
Rohan Maheshwari <rm697@cornell.edu>
2021-10-31 13:32:59 -0400

    lint and format

--- server/scripts/import_roster.ts
+++ server/scripts/import_roster.ts
@@ -1,19 +1,18 @@
 import axios from 'axios';
-import { Course, RosterCourse, RosterResponse } from '../types';
 import fsCoursesRead from 'fs';
 import csv from 'csv-parser';
-import { courses }  from '../server';
-
+import { Course, RosterCourse, RosterResponse } from '../types';
+import { courses } from '../server';
 
 const currSem = 'FA21';
 const prevSem = 'SP21';
 const summerSem = 'SU21';
 
-const classRosterURL = 'https://classes.cornell.edu/api/2.0/search/classes.json?roster=' + currSem + '&subject=';
+const classRosterURL = `https://classes.cornell.edu/api/2.0/search/classes.json?roster=${currSem}&subject=`;
 
-const prevClassRosterURL = 'https://classes.cornell.edu/api/2.0/search/classes.json?roster=' + prevSem + '&subject=';
+const prevClassRosterURL = `https://classes.cornell.edu/api/2.0/search/classes.json?roster=${prevSem}&subject=`;
 
-const summerClassRosterURL = 'https://classes.cornell.edu/api/2.0/search/classes.json?roster=' + summerSem + '&subject='
+const summerClassRosterURL = `https://classes.cornell.edu/api/2.0/search/classes.json?roster=${summerSem}&subject=`;
 
 const coursesCSV = [];
 
@@ -28,101 +27,105 @@ fsCoursesRead
       const fCourse: string[] = coursesCSV[i].tag.split(' ');
       formattedCourses.push(fCourse);
     }
-    //console.log(formattedCourses);
-    getRosterCourses(formattedCourses)
+    // console.log(formattedCourses);
+    getRosterCourses(formattedCourses);
   });
 
-
 async function getRosterCourses(courses: string[][]) {
-    let fetchedCourses = [];
-    let missedCourses = [];
-    for(let i = 0; i < courses.length; i++) {
-        await axios.get(classRosterURL + courses[i][0] + '&q=' + courses[i][1]).then(async (res) => {
-            let resData: RosterResponse  = await res.data as RosterResponse;
-            let classes = resData.data.classes as RosterCourse[];
-            //console.log(classes[0].catalogNbr);
-            fetchedCourses.push(classes[0] as RosterCourse);
-            })
-            .catch(() => {
-                missedCourses.push(courses[i]);
-            })
-    }
-    console.log('i finished getting courses from current roster');
-    transformCourses(fetchedCourses);
-    getPrevRosterCourses(missedCourses);
+  const fetchedCourses = [];
+  const missedCourses = [];
+  for (let i = 0; i < courses.length; i++) {
+    await axios
+      .get(`${classRosterURL + courses[i][0]}&q=${courses[i][1]}`)
+      .then(async (res) => {
+        const resData: RosterResponse = (await res.data) as RosterResponse;
+        const classes = resData.data.classes as RosterCourse[];
+        // console.log(classes[0].catalogNbr);
+        fetchedCourses.push(classes[0] as RosterCourse);
+      })
+      .catch(() => {
+        missedCourses.push(courses[i]);
+      });
+  }
+  console.log('i finished getting courses from current roster');
+  transformCourses(fetchedCourses);
+  getPrevRosterCourses(missedCourses);
 }
 
 async function getPrevRosterCourses(courses: string[][]) {
-    let fetchedCourses = [];
-    let missedCourses = [];
-    for(let i = 0; i < courses.length; i++) {
-        await axios.get(summerClassRosterURL + courses[i][0] + '&q=' + courses[i][1]).then(async (res) => {
-            let resData: RosterResponse  = await res.data as RosterResponse;
-            let classes = resData.data.classes as RosterCourse[];
-            //console.log(classes[0].catalogNbr);
-            fetchedCourses.push(classes[0] as RosterCourse);
-            })
-            .catch(() => {
-                missedCourses.push(courses[i])
-            })
-    }
-    console.log('i finished getting courses from previous roster');
-    transformCourses(fetchedCourses);
-    getSummerRosterCourses(missedCourses);
+  const fetchedCourses = [];
+  const missedCourses = [];
+  for (let i = 0; i < courses.length; i++) {
+    await axios
+      .get(`${summerClassRosterURL + courses[i][0]}&q=${courses[i][1]}`)
+      .then(async (res) => {
+        const resData: RosterResponse = (await res.data) as RosterResponse;
+        const classes = resData.data.classes as RosterCourse[];
+        // console.log(classes[0].catalogNbr);
+        fetchedCourses.push(classes[0] as RosterCourse);
+      })
+      .catch(() => {
+        missedCourses.push(courses[i]);
+      });
+  }
+  console.log('i finished getting courses from previous roster');
+  transformCourses(fetchedCourses);
+  getSummerRosterCourses(missedCourses);
 }
 
 async function getSummerRosterCourses(courses: string[][]) {
-    let fetchedCourses = [];
-    let missedCourses = [];
-    for(let i = 0; i < courses.length; i++) {
-        await axios.get(prevClassRosterURL + courses[i][0] + '&q=' + courses[i][1]).then(async (res) => {
-            let resData: RosterResponse  = await res.data as RosterResponse;
-            let classes = resData.data.classes as RosterCourse[];
-            //console.log(classes[0].catalogNbr);
-            fetchedCourses.push(classes[0] as RosterCourse);
-            })
-            .catch(() => {
-                missedCourses.push(courses[i]);
-            })
-    }
-    console.log('i finished getting');
-    console.log('missed courses after grabbing from summer');
-    console.log(missedCourses);
-    transformCourses(fetchedCourses);
+  const fetchedCourses = [];
+  const missedCourses = [];
+  for (let i = 0; i < courses.length; i++) {
+    await axios
+      .get(`${prevClassRosterURL + courses[i][0]}&q=${courses[i][1]}`)
+      .then(async (res) => {
+        const resData: RosterResponse = (await res.data) as RosterResponse;
+        const classes = resData.data.classes as RosterCourse[];
+        // console.log(classes[0].catalogNbr);
+        fetchedCourses.push(classes[0] as RosterCourse);
+      })
+      .catch(() => {
+        missedCourses.push(courses[i]);
+      });
+  }
+  console.log('i finished getting');
+  console.log('missed courses after grabbing from summer');
+  console.log(missedCourses);
+  transformCourses(fetchedCourses);
 }
 
-
 function transformCourses(courses: RosterCourse[]) {
-    let formattedCourses: Course[] = [];
-    for(let i = 0; i < courses.length; i++) {
-        let formattedSemesters = courses[i].catalogWhenOffered.split(', ');
-        let lastSem = formattedSemesters.pop();
-        lastSem = lastSem.slice(0, lastSem.length - 1);
-        formattedSemesters.push(lastSem);
-        formattedCourses.push({
-            id: courses[i].subject,
-            code: parseInt(courses[i].catalogNbr),
-            content: {
-                title: courses[i].titleLong,
-                description: courses[i].description,
-                courseSite: 'tbd',
-                courseRoster: 'https://classes.cornell.edu/browse/roster/FA21/class/' + courses[i].subject + '/' + courses[i].catalogNbr,
-                credits: courses[i].enrollGroups[0].unitsMaximum,
-                major: 'tbd',
-                designAreas: ['tbd'],
-                semester: formattedSemesters
-            }
-        })
-    }
-    console.log('i finished transforming the courses');
-    pushCoursesToDatabase(formattedCourses);
+  const formattedCourses: Course[] = [];
+  for (let i = 0; i < courses.length; i++) {
+    const formattedSemesters = courses[i].catalogWhenOffered.split(', ');
+    let lastSem = formattedSemesters.pop();
+    lastSem = lastSem.slice(0, lastSem.length - 1);
+    formattedSemesters.push(lastSem);
+    formattedCourses.push({
+      id: courses[i].subject,
+      code: parseInt(courses[i].catalogNbr),
+      content: {
+        title: courses[i].titleLong,
+        description: courses[i].description,
+        courseSite: 'tbd',
+        courseRoster: `https://classes.cornell.edu/browse/roster/FA21/class/${courses[i].subject}/${courses[i].catalogNbr}`,
+        credits: courses[i].enrollGroups[0].unitsMaximum,
+        major: 'tbd',
+        designAreas: ['tbd'],
+        semester: formattedSemesters,
+      },
+    });
+  }
+  console.log('i finished transforming the courses');
+  pushCoursesToDatabase(formattedCourses);
 }
 
 function pushCoursesToDatabase(formattedCourses: Course[]) {
-    for(let i = 0; i < formattedCourses.length; i++) {
-        const courseIdCollection = courses.doc('test').collection(formattedCourses[i].id);
-        const newCourse = courseIdCollection.doc(formattedCourses[i].code.toString());
-        newCourse.set(formattedCourses[i].content);
-    }
-    console.log('i pushed the courses to firestore');
+  for (let i = 0; i < formattedCourses.length; i++) {
+    const courseIdCollection = courses.doc('test').collection(formattedCourses[i].id);
+    const newCourse = courseIdCollection.doc(formattedCourses[i].code.toString());
+    newCourse.set(formattedCourses[i].content);
+  }
+  console.log('i pushed the courses to firestore');
 }
--- server/types.ts
+++ server/types.ts
@@ -67,108 +67,106 @@ export type Event = {
 export const rosterSem = 'SP21';
 
 export type RosterResponse = {
-  status: string,
+  status: string;
   data: {
-    classes: RosterCourse[]
-  },
-}
+    classes: RosterCourse[];
+  };
+};
 
-export type RosterCourse = 
-{
-    strm: number,
-    crseId: number,
-    crseOfferNbr: number,
-    subject: string,
-    catalogNbr: string,
-    titleShort: string,
-    titleLong: string,
-    enrollGroups: EnrollGroup[],
-     description : string,
-     catalogBreadth : string,
-     catalogDistr : string,
-     catalogLang : string,
-     catalogForbiddenOverlaps : string,
-     catalogAttribute : string,
-     catalogWhenOffered : string,
-     catalogComments : string,
-     catalogPrereqCoreq : string,
-     catalogFee : string,
-     catalogSatisfiesReq : string,
-     catalogPermission : string,
-     catalogCourseSubfield : string,
-     catalogOutcomes : any,
-     acadCareer : string,
-     acadGroup : string
-}
+export type RosterCourse = {
+  strm: number;
+  crseId: number;
+  crseOfferNbr: number;
+  subject: string;
+  catalogNbr: string;
+  titleShort: string;
+  titleLong: string;
+  enrollGroups: EnrollGroup[];
+  description: string;
+  catalogBreadth: string;
+  catalogDistr: string;
+  catalogLang: string;
+  catalogForbiddenOverlaps: string;
+  catalogAttribute: string;
+  catalogWhenOffered: string;
+  catalogComments: string;
+  catalogPrereqCoreq: string;
+  catalogFee: string;
+  catalogSatisfiesReq: string;
+  catalogPermission: string;
+  catalogCourseSubfield: string;
+  catalogOutcomes: any;
+  acadCareer: string;
+  acadGroup: string;
+};
 
-export type EnrollGroup =
-    {
-        classSections: ClassSection[],
-        unitsMinimum : number,
-        unitsMaximum : number,
-        componentsOptional : any[],
-        componentsRequired : string[],
-        gradingBasis : string,
-        gradingBasisShort : string,
-        gradingBasisLong : string,
-        simpleCombinations : SimpleCombination[],
-        sessionCode : string,
-        sessionBeginDt : string,
-        sessionEndDt : string,
-        sessionLong : string
-    }
+export type EnrollGroup = {
+  classSections: ClassSection[];
+  unitsMinimum: number;
+  unitsMaximum: number;
+  componentsOptional: any[];
+  componentsRequired: string[];
+  gradingBasis: string;
+  gradingBasisShort: string;
+  gradingBasisLong: string;
+  simpleCombinations: SimpleCombination[];
+  sessionCode: string;
+  sessionBeginDt: string;
+  sessionEndDt: string;
+  sessionLong: string;
+};
 
 export type SimpleCombination = {
-    subject : string,
-    catalogNbr : string,
-    type : string
-}
+  subject: string;
+  catalogNbr: string;
+  type: string;
+};
 
 export type ClassSection = {
-    ssrComponent: string,
-    ssrComponentLong: string,
-    section: string,
-    classNbr: number,
-    meetings: Meeting[],
-    notes: Note[],
-    campus: string,
-    campusDescr: string,
-    location: string,
-    locationDescr: string,
-    startDt: string,
-    endDt : string,
-    addConsent : string,
-    addConsentDescr : string,
-    isComponentGraded : boolean,
-    instructionMode : string,
-    instrModeDescrshort : string,
-    instrModeDescr : string,
-    topicDescription : string
-}
+  ssrComponent: string;
+  ssrComponentLong: string;
+  section: string;
+  classNbr: number;
+  meetings: Meeting[];
+  notes: Note[];
+  campus: string;
+  campusDescr: string;
+  location: string;
+  locationDescr: string;
+  startDt: string;
+  endDt: string;
+  addConsent: string;
+  addConsentDescr: string;
+  isComponentGraded: boolean;
+  instructionMode: string;
+  instrModeDescrshort: string;
+  instrModeDescr: string;
+  topicDescription: string;
+};
 
 export type Meeting = {
-    classMtgNbr: number,
-    timeStart: string,
-    timeEnd: string,
-    startDt: string,
-    endDt: string,
-    instructors: Instructor[],
-    pattern: string,
-    facilityDescr: string,
-    bldgDescr: any,
-    facilityDescrshort: string,
-    meetingTopicDescription: string
-}
+  classMtgNbr: number;
+  timeStart: string;
+  timeEnd: string;
+  startDt: string;
+  endDt: string;
+  instructors: Instructor[];
+  pattern: string;
+  facilityDescr: string;
+  bldgDescr: any;
+  facilityDescrshort: string;
+  meetingTopicDescription: string;
+};
 
 export type Instructor = {
-    instrAssignSeq: number,
-    netid: string,
-    firstName: string,
-    middleName: string,
-    lastName: string
-}
+  instrAssignSeq: number;
+  netid: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
 
 export type Note = {
-    classNotesSeq : number,
-    descrlong : string
-}
+  classNotesSeq: number;
+  descrlong: string;
+};
