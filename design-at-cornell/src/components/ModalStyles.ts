1fcf78a37270afa15983b627f0cfae0b033f1823
Katrina <kw433@cornell.edu>
2021-11-10 14:23:18 -0500

    Modal container

---
+++ design-at-cornell/src/components/ModalStyles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  display: flex;
+  width: 699px;
+  height: 523px;
+`;
--- design-at-cornell/src/explore-courses/courses/CourseBubble.tsx
+++ design-at-cornell/src/explore-courses/courses/CourseBubble.tsx
@@ -8,26 +8,42 @@ import {
 import { courseColors } from '../../constants/colors';
 import bookmark from '../../static/images/bookmark.svg';
 import { Course } from '../../../../server/types';
+import { Modal } from 'semantic-ui-react';
+import { ModalContainer } from '../../components/ModalStyles';
 
-const CourseBubble = (course: Course) => (
-  <ElementContainer
-    style={{ borderColor: courseColors[Math.floor(Math.random() * courseColors.length)] }}
-    onClick={() => (window.location.href = course.content.courseRoster)}
-  >
-    <Subtitle>
-      <p>{course.id + ' ' + course.code}</p>
-      <img src={bookmark} alt="save course" />
-    </Subtitle>
-    <p>{course.content.title}</p>
-    <TagsContainer>
-      <Tag style={{ background: courseColors[Math.floor(Math.random() * courseColors.length)] }}>
-        <p>{course.content.credits + ' Credits'}</p>
-      </Tag>
-      <Tag style={{ background: courseColors[Math.floor(Math.random() * courseColors.length)] }}>
-        <p>{course.content.semester.join(', ')}</p>
-      </Tag>
-    </TagsContainer>
-  </ElementContainer>
-);
+const CourseBubble = (course: Course) => {
+  const [open, setOpen] = React.useState(false);
+
+  const courseCard = (
+    <ElementContainer
+      style={{ borderColor: courseColors[Math.floor(Math.random() * courseColors.length)] }}
+    >
+      <Subtitle>
+        <p>{course.id + ' ' + course.code}</p>
+        <img src={bookmark} alt="save course" />
+      </Subtitle>
+      <p>{course.content.title}</p>
+      <TagsContainer>
+        <Tag style={{ background: courseColors[Math.floor(Math.random() * courseColors.length)] }}>
+          <p>{course.content.credits + ' Credits'}</p>
+        </Tag>
+        <Tag style={{ background: courseColors[Math.floor(Math.random() * courseColors.length)] }}>
+          <p>{course.content.semester.join(', ')}</p>
+        </Tag>
+      </TagsContainer>
+    </ElementContainer>
+  );
+
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={courseCard}
+    >
+      <ModalContainer></ModalContainer>
+    </Modal>
+  );
+};
 
 export default CourseBubble;
