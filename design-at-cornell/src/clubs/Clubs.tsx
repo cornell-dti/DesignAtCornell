9c7222f1888c9e9750f19532476f713dc2a77dd7
Katrina <kw433@cornell.edu>
2021-11-20 02:05:08 -0500

    retrieve clubs

--- design-at-cornell/src/clubs/Clubs.tsx
+++ design-at-cornell/src/clubs/Clubs.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { VerticalFlex } from '../components/ContainerStyles';
+import { Club } from '../../../server/types';
 
 const Clubs = () => {
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/getClubs')
+      .then((res) => res.data.data)
+      .then(setClubs);
+  }, []);
+
+  const [clubs, setClubs] = useState<Club[]>([]);
   return <VerticalFlex></VerticalFlex>;
 };
 
