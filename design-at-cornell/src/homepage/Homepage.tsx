fe239822941544176f0329de6beb0e3363b701c5
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2022-03-19 12:25:20 -0400

    implemented mission statement

--- design-at-cornell/src/homepage/Homepage.tsx
+++ design-at-cornell/src/homepage/Homepage.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import styles from './Homepage.module.css';
 import AppPlaceholderImage from '../placeholder/AppPlaceholderImage';
-
+ 
 const Homepage: React.FC = () => (
   <div className={styles.Homepage} data-testid="Homepage">
-    <AppPlaceholderImage />
+        <div className={styles.Headline}>
+          <b>Your ultimate resource for all things Design at Cornell University.</b>
+        </div>
+        <div className={styles.MissionStat}>
+          Design at Cornell Mission Statement
+        </div>
   </div>
 );
-
+ 
 export default Homepage;
