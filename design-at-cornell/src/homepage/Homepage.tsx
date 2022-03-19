602d478563bb342a82e67bc3d0ee846ac8c38fc1
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2022-03-19 12:42:50 -0400

    removed unused AppPlaceholder import

--- design-at-cornell/src/homepage/Homepage.tsx
+++ design-at-cornell/src/homepage/Homepage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Homepage.module.css';
-import AppPlaceholderImage from '../placeholder/AppPlaceholderImage';
 
 const Homepage: React.FC = () => (
   <div className={styles.Homepage} data-testid="Homepage">
