b44d0ec374005403c86674c8a3b25bce7e7aef01
Sydney Tran <sydney.tran@live.com>
2022-03-20 17:54:05 -0400

    remove filter bar from student orgs page

--- design-at-cornell/src/clubs/title/Title.tsx
+++ design-at-cornell/src/clubs/title/Title.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import FilterBar, { Props } from './FilterBar';
 import { TitleBackground, TitleContainer } from '../../components/TitleStyles';
 import { colors } from '../../constants/colors';
 
-const Title = (props: Props) => (
+const Title = () => (
   <TitleBackground color={colors.cyan}>
     <TitleContainer>
       <h1>Student Organizations</h1>
       <p>Find your design people, your creative community :)</p>
     </TitleContainer>
-    <FilterBar {...props} />
   </TitleBackground>
 );
 
