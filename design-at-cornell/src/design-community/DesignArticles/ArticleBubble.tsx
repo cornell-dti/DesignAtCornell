ca1f652fff1226d7474442a371b08059c123126d
Sydney Tran <sydney.tran@live.com>
2022-04-26 13:28:41 -0400

    begin article cards

--- design-at-cornell/src/design-community/DesignArticles/ArticleBubble.tsx
+++ design-at-cornell/src/design-community/DesignArticles/ArticleBubble.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import {
   ElementContainer,
+  Title,
+  Subtitle
 } from '../../components/DashboardElementStyles';
 import { Article } from '../../../../server/types';
 
 const ArticleBubble = (article: Article) => (
   <ElementContainer>
+    <Title>
+      <p>{article.title}</p>
+    </Title>
+    <Subtitle>
+      <p>{article.content_text}</p>
+    </Subtitle>
   </ElementContainer>
 );
 
