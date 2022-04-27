1a9959bfafef1a15abc25f457dafa72380ccc266
Sydney Tran <sydney.tran@live.com>
2022-04-27 18:31:41 -0400

    implement design of article cards

--- design-at-cornell/src/design-community/design-articles/ArticleBubble.tsx
+++ design-at-cornell/src/design-community/design-articles/ArticleBubble.tsx
@@ -5,15 +5,20 @@ import {
   Subtitle
 } from '../../components/DashboardElementStyles';
 import { Article } from '../../../../server/types';
+import { VerticalFlex } from '../../components/ContainerStyles';
+
 
 const ArticleBubble = (article: Article) => (
-  <ElementContainer>
-    <Title>
-      <p>{article.title}</p>
-    </Title>
-    <Subtitle>
-      <p>{article.content_text}</p>
-    </Subtitle>
+  <ElementContainer style={{width: `370px`, height: `350px`, borderRadius: `20px`}}>
+    <VerticalFlex>
+      <img src={article.image_featured} alt={article.image_alt}/>
+      <Title>
+        <p>{article.title}</p>
+      </Title>
+      <Subtitle>
+        <p style={{marginBottom: `-5px`, fontSize: `12px`, lineHeight: `18px`}}>{article.content_text}</p>
+      </Subtitle>
+    </VerticalFlex>
   </ElementContainer>
 );
 
