880e04dec8c128d207894ffa2d78c2948ff7351e
Sydney Tran <sydney.tran@live.com>
2022-04-28 15:05:01 -0400

    fix formatting

--- design-at-cornell/src/design-community/Title.tsx
+++ design-at-cornell/src/design-community/Title.tsx
@@ -7,6 +7,7 @@ const Title = () => (
     <TitleContainerBox>
       <h1>Welcome to your design community!</h1>
       <p>
+        {' '}
         Grow relationships with like-minded individuals by connecting with the amazing design
         faculty at Cornell! Expand your knowledge of design through exploring relevant articles.
       </p>
