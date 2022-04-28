80a9d1585712dffd42d37c13eefa7d99f6cf9e24
Sydney Tran <sydney.tran@live.com>
2022-04-28 15:54:27 -0400

    rename faculty folder name

--- design-at-cornell/src/design-community/Faculty/FacultyBubble.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ElementContainer } from '../../components/DashboardElementStyles';
-import { Instructor } from '../../../../server/types';
-
-const FacultyBubble = (faculty: Instructor) => (
-  <ElementContainer style={{ boxShadow: `0px 0px` }}></ElementContainer>
-);
-
-export default FacultyBubble;
