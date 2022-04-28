d72c923c3439b42de57ab36d59b03bd5473604cf
Sydney Tran <sydney.tran@live.com>
2022-04-27 23:13:00 -0400

    implement top large article card

--- design-at-cornell/src/design-community/design-articles/articles-page/Dashboard.tsx
+++ design-at-cornell/src/design-community/design-articles/articles-page/Dashboard.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { VerticalFlex } from '../../../components/ContainerStyles';
+import { HorizontalFlex, VerticalFlex } from '../../../components/ContainerStyles';
 import { Grid } from '../../../components/DashboardGridStyles';
+import { HeadingLine, HeadingTitle } from '../../../components/DashboardElementStyles';
 import { Article } from '../../../../../server/types';
 import ArticleBubble from './Bubble';
-
+import LargeArticleBubble from './LargeBubble';
 const Dashboard = (article: Article[]) => {
+
   const articleBubbles = (
     <Grid style = {{justifyContent: `space-between`}}>
-      {Object.values(article).map((article) => (
+      {Object.values(article).slice(0,1).map((article) => (
+        <LargeArticleBubble key={article.id} {...article} />
+      ))}
+      {Object.values(article).slice(1).map((article) => (
         <ArticleBubble key={article.id} {...article} />
       ))}
     </Grid>
@@ -17,7 +22,15 @@ const Dashboard = (article: Article[]) => {
     <VerticalFlex>
       <br></br>
       <br></br>
+      <HorizontalFlex>
+        <HeadingLine style={{width: `75px`}}/>
+        <HeadingTitle style={{fontWeight: 700, fontSize: `45px`}}>Articles</HeadingTitle>
+        <HeadingLine style={{width: `1150px`}}/>
+      </HorizontalFlex>
+      <br></br>
+      <br></br>
       {articleBubbles}
+      <br></br>
     </VerticalFlex>
   );
 };
