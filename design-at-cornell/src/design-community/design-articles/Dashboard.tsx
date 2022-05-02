b8d4cd15c702f405a7a2c434dfb6a7f34e77be65
Sydney Tran <sydney.tran@live.com>
2022-05-02 14:09:11 -0400

    rename community grid style

--- design-at-cornell/src/design-community/design-articles/Dashboard.tsx
+++ design-at-cornell/src/design-community/design-articles/Dashboard.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { HorizontalFlex, VerticalFlex } from '../../components/ContainerStyles';
-import { ArticleGrid } from '../../components/DashboardGridStyles';
+import { CommunityGrid } from '../../components/DashboardGridStyles';
 import { HeadingLine } from '../../components/DashboardElementStyles';
 import { Article } from '../../../../server/types';
 import ArticleBubble from './ArticleBubble';
 
 const Dashboard = (article: Article[]) => {
   const articleBubbles = (
-    <ArticleGrid>
+    <CommunityGrid>
       {Object.values(article)
         .slice(0, 3)
         .map((article) => (
           <ArticleBubble key={article.id} {...article} />
         ))}
-    </ArticleGrid>
+    </CommunityGrid>
   );
 
   return (
