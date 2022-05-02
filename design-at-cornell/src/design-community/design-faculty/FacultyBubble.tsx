e312e676c7164dd4e35c6bacfacf95d90daffdeb
Sydney Tran <sydney.tran@live.com>
2022-05-02 16:23:54 -0400

    fix formatting

--- design-at-cornell/src/design-community/design-faculty/FacultyBubble.tsx
+++ design-at-cornell/src/design-community/design-faculty/FacultyBubble.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {
-    FacultyBubbleContainer,
-  } from '../../components/DashboardElementStyles';
+import { FacultyBubbleContainer } from '../../components/DashboardElementStyles';
 import { Faculty } from '../../../../server/types';
 import { VerticalFlex } from '../../components/ContainerStyles';
 
 const FacultyBubble = (faculty: Faculty) => (
-    <FacultyBubbleContainer onClick={() => window.open(faculty.website)}>
+  <FacultyBubbleContainer onClick={() => window.open(faculty.website)}>
     <VerticalFlex>
-      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="faculty image"/>
-        <p>Professor's Full Name</p>
-        <p>Job Title</p>
-        <p>Department</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        alt="faculty image"
+      />
+      <p>Professor's Full Name</p>
+      <p>Job Title</p>
+      <p>Department</p>
     </VerticalFlex>
   </FacultyBubbleContainer>
 );
