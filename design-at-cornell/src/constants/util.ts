ed67ea07b967c8ed7869a92ccb8c9a05eb10e829
Gavin Fogel <gavin@fogel.io>
2022-05-06 01:13:06 -0400

    Remove temporary env var bypass

--- design-at-cornell/src/constants/util.ts
+++ design-at-cornell/src/constants/util.ts
@@ -4,9 +4,7 @@ import axios from 'axios';
 dotenv.config();
 
 const api = axios.create({
-  // TODO: Revert me
-  baseURL: 'http://design.cornell.edu/',
-  // baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export default api;
