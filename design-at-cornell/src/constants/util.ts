136bedaf02d7f3da3956ac2105796a3e3510b90f
Will Zhang <wz282@cornell.edu>
2022-09-24 12:54:42 -0400

    ignore firebase.json + frontend endpoint

--- design-at-cornell/src/constants/util.ts
+++ design-at-cornell/src/constants/util.ts
@@ -3,8 +3,12 @@ import axios from 'axios';
 
 dotenv.config();
 
+// PROD: process.env.REACT_APP_BASE_URL
+// LOCAL: http://localhost:3000
+// Change to only local if you are testing backend functionality.
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost:3000',
 });
 
 export default api;
