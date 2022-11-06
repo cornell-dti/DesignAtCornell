a77d9f5def0f3920821c1f997cf7d6b4ef0acccf
Sydney <sydney.tran@live.com>
2022-11-05 22:51:12 -0400

    Add loading spinner to events page

--- design-at-cornell/src/events/Events.tsx
+++ design-at-cornell/src/events/Events.tsx
@@ -1,22 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { VerticalFlex, ShowContainer } from '../components/ContainerStyles';
+import { VerticalFlex, ShowContainer, Spinner } from '../components/ContainerStyles';
 import { EventList } from './EventsStyles';
 import Title from './Title';
 import Filter from './dashboard/Filter';
 import EventDisplay from './dashboard/EventDisplay';
-import { Event } from '../../../server/src/types';
+import { LoadingEvents } from '../../../server/src/types';
 import api from '../constants/util';
 import Pagination from '../pagination/Pagination';
 
 const Events = () => {
+  const [state, setState] = useState<LoadingEvents>({
+    loading: false,
+    events: [],
+  });
+
   useEffect(() => {
+    setState({ loading: true, events: [] });
     api
       .get('/getEvents')
       .then((res) => res.data)
-      .then(FormatEvents);
+      .then((events) => setState({ loading: false, events: events }));
   }, []);
 
-  const [events, FormatEvents] = useState<Event[]>([]);
   const date = new Date();
   const [year, setYear] = useState(date.getFullYear());
   const [month, setMonth] = useState(date.getMonth());
@@ -26,7 +31,7 @@ const Events = () => {
   const rightClickHandler = () => setYear(year + 1);
   const monthClickHandler = (m: number) => setMonth(m);
 
-  const filteredEvents = events.filter((event) => {
+  const filteredEvents = state.events.filter((event) => {
     const eventDate = new Date(event.date);
     const eventMonth = eventDate.getFullYear() * 12 + eventDate.getMonth();
     const filterMonth = year * 12 + month;
@@ -46,26 +51,39 @@ const Events = () => {
     </EventList>
   );
 
+  const LoadingIndicator = () => {
+    return <Spinner />;
+  };
+
+  const EventsPage = () => {
+    return (
+      <VerticalFlex>
+        <Filter
+          month={month}
+          year={year}
+          leftClickHandler={leftClickHandler}
+          rightClickHandler={rightClickHandler}
+          monthClickHandler={monthClickHandler}
+          setPage={setPage}
+        />
+        {eventDisplay}
+        <ShowContainer show={filteredEvents.length - eventsPerPage > 0}>
+          <Pagination
+            currentPage={currentPage}
+            cardsPerPage={eventsPerPage}
+            totalCards={filteredEvents.length}
+            paginate={setPage}
+          />
+        </ShowContainer>
+      </VerticalFlex>
+    );
+  };
+
   return (
     <VerticalFlex>
       <Title />
-      <Filter
-        month={month}
-        year={year}
-        leftClickHandler={leftClickHandler}
-        rightClickHandler={rightClickHandler}
-        monthClickHandler={monthClickHandler}
-        setPage={setPage}
-      />
-      {eventDisplay}
-      <ShowContainer show={filteredEvents.length - eventsPerPage > 0}>
-        <Pagination
-          currentPage={currentPage}
-          cardsPerPage={eventsPerPage}
-          totalCards={filteredEvents.length}
-          paginate={setPage}
-        />
-      </ShowContainer>
+      {state.loading && <LoadingIndicator />}
+      {!state.loading && <EventsPage />}
     </VerticalFlex>
   );
 };
